b5147278f81a689f2c770a8970583f6175dd0359
三上 哲朗 <mickamy9@gmail.com>
2015-05-10 01:50:08 +0900

    新規授業追加ダイアログ作成。しかし、追加分が画面に反映されないので修正必要。

--- app/src/main/java/jp/techinstitute/ti_046/timetablefailure/MainActivity.java
+++ app/src/main/java/jp/techinstitute/ti_046/timetablefailure/MainActivity.java
@@ -1,18 +1,26 @@
 package jp.techinstitute.ti_046.timetablefailure;
 
 import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.text.SpannableStringBuilder;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.ViewGroup;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class MainActivity extends ActionBarActivity {
+
+    private TableHelper helper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        helper = new TableHelper(MainActivity.this);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.container, new MainFragment())
@@ -37,9 +45,32 @@ public class MainActivity extends ActionBarActivity {
                 // AlertDialog to create new ClassTable
                 AlertDialog.Builder builder = new AlertDialog.Builder(this);
                 // set dialog layout of add_fragment_dialog
-                ViewGroup container = (ViewGroup) findViewById(R.id.add_fragment_container);
-                builder.setView(getLayoutInflater().inflate(R.layout.fragment_add_dialog, container));
+                View rootView = getLayoutInflater().inflate(R.layout.fragment_add_dialog, null);
+                // get views in rootView
+                // findViewByIdはBuilder.setView後は使えないので要注意
+                final Spinner spinnerDay = (Spinner) rootView.findViewById(R.id.spinner_day);
+                final Spinner spinnerTime = (Spinner) rootView.findViewById(R.id.spinner_time);
+                final EditText editName = (EditText) rootView.findViewById(R.id.edit_name);
+                final EditText editTeacher = (EditText) rootView.findViewById(R.id.edit_teacher);
+
+                builder.setView(rootView);
                 builder.setTitle("新規授業登録");
+                builder.setPositiveButton("追加", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        ClassTable classTable = new ClassTable();
+                        classTable.setDay(getSpinnerItemSelected(spinnerDay));
+                        classTable.setTime(getSpinnerItemSelected(spinnerTime));
+                        classTable.setName(getStringOfEditText(editName));
+                        classTable.setTeacher(getStringOfEditText(editTeacher));
+                        helper.createClassTable(classTable, helper.getWritableDatabase());
+
+                        // TODO: もし追加した授業がその日の最初の授業なら、アラームセットするかダイアログ
+
+                        Toast.makeText(MainActivity.this, classTable.getName()
+                                + "が時間割に追加されました。", Toast.LENGTH_SHORT).show();
+                    }
+                });
                 builder.show();
                 break;
             case R.id.action_drop_class_table:
@@ -48,5 +79,14 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private String getSpinnerItemSelected(Spinner spinner) {
+        return spinner.getSelectedItem().toString();
+    }
+
+    private String getStringOfEditText(EditText editText) {
+        SpannableStringBuilder spannableStringBuilder = (SpannableStringBuilder) editText.getText();
+        return spannableStringBuilder.toString();
     }
 }
